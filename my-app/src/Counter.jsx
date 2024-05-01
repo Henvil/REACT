f36686c741a2c523ca1f2e5ab6ffa5fdc6b8fd43
Henvil <turningsheeps@gmail.com>
2024-05-01 19:26:08 +0200

    done Exercise1

--- my-app/src/Counter.jsx
+++ my-app/src/Counter.jsx
@@ -1,19 +1,12 @@
-/*
-    Create a Counter function component and create a new state variable called counter initialized to 0. 
-    The Counter component should render the counter value within an h2 tag, and the counter value should 
-    be incremented every time the user clicks on a button.
-    Tip: use the useState hook. 
-    Make it so that the initial value of the counter and the increment amount are passed as props 
-    to the component. 
-    When calling "setter" function to increment the counter, should the parameter be 
-    a function or an immediate value? Why?
-
-*/
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { CounterDisplay } from './Counterdisplay';
 
 export function Counter({ initialValue, incrementAmount }) {
   const [counter, setCounter] = useState(initialValue);
+
+  useEffect(() => {
+    console.log('The current counter value is:', counter);
+  }, [counter]);
 
   function handleCounterIncrease() {
     setCounter((c) => c + incrementAmount);
