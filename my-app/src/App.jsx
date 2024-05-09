57f3bf56253b09b44869d224eb640f08f70c0511
Henvil <turningsheeps@gmail.com>
2024-05-09 12:48:08 +0200

    refs-counter init

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,12 +1,10 @@
-import { FocusableInput } from './FocusableInput';
-import MessageRefs from './MessageRefs';
+import { Counter } from './Counter';
 
 export function App() {
   return (
     <div>
       <h1>Questa è un esempio di App</h1>
-      <FocusableInput />
-      <MessageRefs />
+      <Counter initialValue={0} incrementAmount={2} />
     </div>
   );
 }
--- my-app/src/Counter.jsx
+++ my-app/src/Counter.jsx
@@ -1,11 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import { CounterDisplay } from './Counterdisplay';
+import { useRef } from 'react';
 
 export function Counter({ initialValue, incrementAmount }) {
   const [counter, setCounter] = useState(initialValue);
+  const directionRef = useRef(null);
+  const prevCounterRef = useRef(initialValue);
 
   useEffect(() => {
     console.log('The current counter value is:', counter);
+
+    if (counter > prevCounterRef.current) {
+      directionRef.current = 'Valore aumentato.';
+    } else if (counter < prevCounterRef.current) {
+      directionRef.current = 'Valore diminuito.';
+    }
+    prevCounterRef.current = counter;
+
+    if (directionRef.current !== null) {
+      console.log('Cambiamento:', directionRef.current);
+    }
   }, [counter]);
 
   function handleCounterIncrease() {
